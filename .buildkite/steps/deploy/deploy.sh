0ab9d65cedfa39eac6496bc696d0a7ae8bcc8168
Renato Teixeira <rena2damas@gmail.com>
2021-08-23 17:10:36 +0100

    ops(kustomization): updates #wip

--- .buildkite/steps/deploy/deploy.sh
+++ .buildkite/steps/deploy/deploy.sh
@@ -2,3 +2,18 @@
 
 set -euo pipefail
 
+# validate env parameters
+if [[ ! -v NAMESPACE ]]; then
+  echo "--- :boom: Missing 'NAMESPACE'" 1>&2
+  exit 1
+fi
+
+# temporary files
+tmpdir=$(mktemp -d)
+manifest="${tmpdir}/manifest.yaml"
+
+# setup kustomization file
+NAMESPACE="$NAMESPACE" \
+  envsubst <"$(dirname "$0")/templates/kustomization.yaml" >"${manifest}"
+
+kubectl kustomize "$manifest" | kubectl replace -f -
