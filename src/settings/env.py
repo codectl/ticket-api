e3b1f84f98aac9ef3c91492a3ad7604c8574323d
Renato Teixeira <rena2damas@gmail.com>
2022-07-15 21:57:16 +0100

    refactor: add env settings #wip

---
+++ src/settings/env.py
@@ -0,0 +1,21 @@
+import environs
+
+
+def create_env():
+    dotenv = environs.Env()
+    return dotenv
+
+
+def load_dotenv(dotenv: bool):
+    """Load enviroment from given file or dict."""
+    if dotenv:
+        env.read_env(".env")
+
+
+def config_class(environment: str):
+    """Link given environment to a config class."""
+    return f"{__package__}.config.{environment.capitalize()}Config"
+
+
+# the application environment
+env = create_env()
