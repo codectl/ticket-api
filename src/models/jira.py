83914c2fa5c22d5bcfec63e60d73abf5c44f4d65
Renato Teixeira <rena2damas@gmail.com>
2022-07-18 15:18:37 +0100

    refactor: rename model modules #wip

--- src/models/jira.py
+++ src/models/jira.py
@@ -1,13 +1,14 @@
 class Board:
-    def __init__(self, key: str = None, raw: dict = None):
+    def __init__(self, key: str = None, raw: dict = None, is_default: bool = False):
         self.key = key
+        self.raw = raw
+        self.is_default = is_default
         self.id = raw["id"]
         self.name = raw["name"]
-        self.raw = raw
 
     @property
     def filter(self):
         # prevent import loop
         from src.services.jira import JiraSvc
 
-        return JiraSvc().get_board_filter(board_id=self.id)
+        return JiraSvc().board_filter(board_id=self.id)
