282d6cb31b39604c98e8e79b5306ca9e2adae76f
Renato Teixeira <rena2damas@gmail.com>
2022-07-18 15:20:46 +0100

    refactor: rewrite dict as literal #wip

--- src/schemas/deserializers/CreateTicket.py
+++ src/schemas/deserializers/CreateTicket.py
@@ -1,50 +1,48 @@
 from flask import current_app
 from marshmallow import Schema, fields, validate
 
-from src.models.jira.Board import Board
-from src.services.jira import JiraService
+from src.services.jira import JiraSvc
 
 
 class CreateTicketSchema(Schema):
     title = fields.String(
         required=True,
-        metadata=dict(description="an intuitive title for the ticket", example=""),
+        metadata={"description": "an intuitive title for the ticket", "example": ""},
         validate=validate.Length(min=5),
     )
     body = fields.String(
         required=True,
-        metadata=dict(description="the content of the ticket", example=""),
+        metadata={"description": "the content of the ticket", "example": ""},
         validate=validate.Length(min=5),
     )
     reporter = fields.Email(
-        required=True, metadata=dict(description="ticket reporter email", example="")
+        required=True, metadata={"description": "ticket reporter email", "example": ""}
     )
     board = fields.String(
         required=True,
-        metadata=dict(
-            description="boards to fetch tickets from", default=Board.default().key
-        ),
-        validate=validate.OneOf(JiraService.supported_board_keys()),
+        metadata={
+            "description": "boards to fetch tickets from",
+            "default": JiraSvc.default_board(),
+        },
+        validate=validate.OneOf(b.key for b in JiraSvc().boards()),
     )
     category = fields.String(
         required=True,
-        metadata=dict(
-            description="category the ticket belongs to",
-            default=current_app.config["JIRA_TICKET_LABEL_DEFAULT_CATEGORY"],
-        ),
-        validate=validate.OneOf(JiraService.supported_categories()),
+        metadata={
+            "description": "category the ticket belongs to",
+            "default": current_app.config["JIRA_TICKET_LABEL_DEFAULT_CATEGORY"],
+        },
+        validate=validate.OneOf(JiraSvc.supported_categories()),
     )
     watchers = fields.List(
         fields.Email(),
         allow_none=False,
         load_default=[],
-        metadata=dict(description="users to be watching the ticket", example=[]),
+        metadata={"description": "users to be watching the ticket", "example": []},
     )
     priority = fields.String(
         validate=validate.OneOf(["low", "high"]),
         allow_none=True,
         load_default=None,
-        metadata=dict(
-            description="define a higher or lower ticket priority", example=None
-        ),
+        metadata={"description": "define a higher or lower ticket priority"},
     )
