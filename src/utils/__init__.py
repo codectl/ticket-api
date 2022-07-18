64344f66e7c97a9ebedf4035fc839272ae035405
Renato Teixeira <rena2damas@gmail.com>
2022-07-18 10:18:57 +0100

    refactor: add http utilities #wip

---
+++ src/utils/__init__.py
@@ -0,0 +1,18 @@
+from flask_restful import abort
+from werkzeug.http import HTTP_STATUS_CODES
+
+from src.schemas.serlializers.http import HttpResponseSchema
+from src.settings import oas
+
+
+def http_response(code: int, message="", serialize=True, **kwargs):
+    response = oas.HttpResponse(
+        code=code, reason=HTTP_STATUS_CODES[code], message=message
+    )
+    if serialize:
+        return HttpResponseSchema(**kwargs).dump(response)
+    return response
+
+
+def abort_with(code: int, message=""):
+    abort(code, **http_response(code, message=message))
