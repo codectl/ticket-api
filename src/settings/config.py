d7fac98ed192eafe34feb3be80e78902d4a408dc
renatodamas <rena2damas@gmail.com>
2022-08-25 16:09:51 +0100

    refactor: set default values to all configs #wip

--- src/settings/config.py
+++ src/settings/config.py
@@ -14,30 +14,33 @@ class BaseConfig:
     APPLICATION_CONTEXT = env("APPLICATION_CONTEXT", "/")
 
     # Database settings
-    SQLALCHEMY_DATABASE_URI = env("SQLALCHEMY_DATABASE_URI")
+    SQLALCHEMY_DATABASE_URI = env("SQLALCHEMY_DATABASE_URI", None)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DATABASE_CONNECT_OPTIONS = {}
+
+    # OpenAPI supported version
+    OPENAPI = env("OPENAPI", "3.0.3")
 
     # The application providing info about the ticket
     TICKET_CLIENT_APP = env("TICKET_CLIENT_APP", "localhost")
 
     # The mailbox to manage
-    MAILBOX = env("MAILBOX")
+    MAILBOX = env("MAILBOX", None)
 
     # Office365 related configurations #
     O365_URL = env("O365_URL", "https://login.microsoftonline.com")
 
     # Office365 registered tenant
-    O365_TENANT_ID = env("O365_TENANT_ID")
+    O365_TENANT_ID = env("O365_TENANT_ID", None)
 
-    # O365 client Id according to OAuth2 standards
-    O365_CLIENT_ID = env("O365_CLIENT_ID")
+    # O365 client id according to OAuth2 standards
+    O365_CLIENT_ID = env("O365_CLIENT_ID", None)
 
     # O365 client secret according to OAuth2 standards
-    O365_CLIENT_SECRET = env("O365_CLIENT_SECRET")
+    O365_CLIENT_SECRET = env("O365_CLIENT_SECRET", None)
 
     # O365 scopes according to OAuth2 standards
-    O365_SCOPES = env.list("O365_SCOPES")
+    O365_SCOPES = env.list("O365_SCOPES", [])
 
     # Streaming connection settings
     # See https://bit.ly/3eqDsGs for details
@@ -45,9 +48,9 @@ class BaseConfig:
     KEEP_ALIVE_INTERVAL_IN_SECONDS = env.int("KEEP_ALIVE_INTERVAL_IN_SECONDS", 300)
 
     # Atlassian credentials
-    ATLASSIAN_URL = env("ATLASSIAN_URL")
-    ATLASSIAN_USER = env("ATLASSIAN_USER")
-    ATLASSIAN_API_TOKEN = env("ATLASSIAN_API_TOKEN")
+    ATLASSIAN_URL = env("ATLASSIAN_URL", "https://atlassian.net")
+    ATLASSIAN_USER = env("ATLASSIAN_USER", None)
+    ATLASSIAN_API_TOKEN = env("ATLASSIAN_API_TOKEN", None)
 
     # Jira settings
     JIRA_TICKET_TYPE = env("JIRA_TICKET_TYPE", None)
@@ -57,7 +60,7 @@ class BaseConfig:
 
     # Jira boards to fetch tickets from
     JIRA_BOARDS = env.list("JIRA_BOARDS", [])
-    JIRA_DEFAULT_BOARD = env("JIRA_DEFAULT_BOARD")
+    JIRA_DEFAULT_BOARD = env("JIRA_DEFAULT_BOARD", None)
 
     # Filter settings
     EMAIL_WHITELISTED_DOMAINS = env.list("EMAIL_WHITELISTED_DOMAINS", [])
