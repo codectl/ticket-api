357949a0e1958551c15096d5191323e139a00b81
renatodamas <rena2damas@gmail.com>
2022-08-26 12:03:23 +0100

    test: add case for mypermissions #wip

--- tests/unit/test_jira.py
+++ tests/unit/test_jira.py
@@ -17,7 +17,7 @@ def svc(adapter):
         url="https://jira.atlassian.com",
         user="test",
         token="xxx",
-        get_server_info=False
+        get_server_info=False,
     )
     session = requests.Session()
     session.mount("https://", adapter)
@@ -37,3 +37,19 @@ class TestJiraSvc:
 
         mocker.patch.object(svc, "issue", side_effect=jira.JIRAError(status_code=404))
         assert svc.exists_issue("Jira-123") is False
+
+    def test_has_permissions(self, svc, adapter):
+        path = svc._get_url("mypermissions")
+        data = {"permissions": {}}
+        adapter.register_uri("GET", path, json=data)
+        assert svc.has_permissions(permissions=[]) is True
+        assert svc.has_permissions(permissions=["perm"]) is False
+        data["permissions"]["perm1"] = {"havePermission": True}
+        adapter.register_uri("GET", path, json=data)
+        assert svc.has_permissions(permissions=["perm1"]) is True
+        assert svc.has_permissions(permissions=["perm1", "perm2"]) is False
+        data["permissions"]["perm2"] = {"havePermission": False}
+        adapter.register_uri("GET", path, json=data)
+        assert svc.has_permissions(permissions=["perm1"]) is True
+        assert svc.has_permissions(permissions=["perm2"]) is False
+        assert svc.has_permissions(permissions=["perm1", "perm2"]) is False
