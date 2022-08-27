04b02d16a52b3069b69bf3a9160c10a95570aef3
renatodamas <rena2damas@gmail.com>
2022-08-27 12:26:42 +0100

    test(functional): test issue existance #wip

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -1,15 +1,24 @@
 import pytest
+import unittest
 
 from src.services.jira import JiraSvc
 from src.settings.env import env, load_dotenv
 
 
-@pytest.fixture(scope="module")
-def svc(app):
+@pytest.fixture(scope="class")
+def svc(app, request):
     load_dotenv(True)
     svc = JiraSvc(
         url=env.str("CI_JIRA_CLOUD_URL"),
         user=env.str("CI_JIRA_CLOUD_USER"),
         token=env.str("CI_JIRA_CLOUD_TOKEN"),
     )
+    request.cls.svc = svc
     return svc
+
+
+@pytest.mark.usefixtures("svc")
+class JiraTestCase(unittest.TestCase):
+    def setUp(self):
+        self.project = self.svc.project(env.str("CI_JIRA_CLOUD_PROJECT_KEY"))
+        self.issue_type = env.str("CI_JIRA_ISSUE_TYPE")
--- tests/functional/test_jira.py
+++ tests/functional/test_jira.py
@@ -1,2 +1,15 @@
-class TestJiraSvc:
-    pass
+from tests.functional.conftest import JiraTestCase
+
+
+class TestJiraSvc(JiraTestCase):
+    def test_exists_issue(self):
+        issue = self.svc.create_issue(
+            summary="Some dummy issue",
+            project=self.project.key,
+            issuetype=self.issue_type,
+        )  # create dummy issue
+
+        assert self.svc.exists_issue(issue.id)
+        issue.delete()
+        assert not self.svc.exists_issue(issue.id)
+        assert not self.svc.exists_issue("000")
