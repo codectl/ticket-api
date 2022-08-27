0e1e53470818584432ecc82b7f6e6ef3c9a31bb1
renatodamas <rena2damas@gmail.com>
2022-08-27 10:30:08 +0100

    test: set conftest global #wip

---
+++ tests/conftest.py
@@ -0,0 +1,25 @@
+import pytest
+
+from src.app import create_app
+
+
+@pytest.fixture(scope="module")
+def app():
+    app = create_app(
+        config_name="testing",
+        dotenv=False,
+        configs={
+            "SQLALCHEMY_DATABASE_URI": "sqlite:///:memory:",
+            "FLASK_RUN_HOST": "localhost",
+            "FLASK_RUN_PORT": 5000,
+            "APPLICATION_ROOT": "/",
+            "OPENAPI": "3.0.3",  # default version
+        },
+    )
+    with app.test_request_context():
+        yield app
+
+
+@pytest.fixture(scope="module")
+def client(app):
+    return app.test_client()
