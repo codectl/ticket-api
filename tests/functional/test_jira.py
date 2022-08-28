b591d519bb0a72223e3b84d784e85d8cd4818325
renatodamas <rena2damas@gmail.com>
2022-08-28 08:40:31 +0100

    test: additional tests #wip

--- tests/functional/test_jira.py
+++ tests/functional/test_jira.py
@@ -1,7 +1,21 @@
+import jira
+import pytest
+
 from tests.functional.conftest import JiraTestCase
 
 
 class TestJiraSvc(JiraTestCase):
+    def test_boards(self):
+        boards = self.svc.boards()
+        assert len(boards) == 1
+        assert boards[0].name == "CI board"
+        assert boards[0].is_default is True
+
+    def test_jql_query(self):
+        boards = self.svc.boards()
+        query = self.svc.create_jql_query(board_keys=(board.key for board in boards))
+        assert "filter in" in query
+
     def test_exists_issue(self):
         issue = self.svc.create_issue(
             summary="Some dummy issue",
@@ -19,5 +33,9 @@ class TestJiraSvc(JiraTestCase):
         assert self.svc.has_permissions(["INVALID_PERMISSION"]) is False
 
     def test_board_configuration(self):
-        print(self.svc.boards())
-        assert False
+        config = self.svc.board_configuration(board_id=self.board.id)
+        assert "filter" in config
+        assert "location" in config
+        with pytest.raises(jira.JIRAError) as ex:
+            self.svc.board_configuration(board_id="000")
+            ex.status_code = 404
