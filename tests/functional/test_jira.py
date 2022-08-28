fe1aae584f24ec52930cb4d829c5ab5b0baa2a07
renatodamas <rena2damas@gmail.com>
2022-08-28 16:11:46 +0100

    test: test add attachment

--- tests/functional/test_jira.py
+++ tests/functional/test_jira.py
@@ -1,31 +1,29 @@
+import io
+
 import jira
 import pytest
+import werkzeug.datastructures
 
 from tests.functional.conftest import JiraTestCase
 
 
+@pytest.fixture
+def issue(svc, board, issue_type, request):
+    issue = svc.create_issue(
+        summary="Some dummy issue",
+        project=board.project,
+        issuetype=issue_type,
+    )
+    request.cls.issue = issue
+    return issue
+
+
 class TestJiraSvc(JiraTestCase):
-    def test_boards(self):
-        boards = self.svc.boards()
-        assert len(boards) == 1
-        assert boards[0].name == "CI board"
-        assert boards[0].is_default is True
-
-    def test_jql_query(self):
-        boards = self.svc.boards()
-        query = self.svc.create_jql_query(board_keys=(board.key for board in boards))
-        assert "filter in" in query
-
+    @pytest.mark.usefixtures("issue")
     def test_exists_issue(self):
-        issue = self.svc.create_issue(
-            summary="Some dummy issue",
-            project=self.project.key,
-            issuetype=self.issue_type,
-        )  # create dummy issue
-
-        assert self.svc.exists_issue(issue.id) is True
-        issue.delete()
-        assert self.svc.exists_issue(issue.id) is False
+        assert self.svc.exists_issue(self.issue.id) is True
+        self.issue.delete()
+        assert self.svc.exists_issue(self.issue.id) is False
         assert self.svc.exists_issue("000") is False
 
     def test_has_permissions(self):
@@ -39,3 +37,28 @@ class TestJiraSvc(JiraTestCase):
         with pytest.raises(jira.JIRAError) as ex:
             self.svc.board_configuration(board_id="000")
             ex.status_code = 404
+
+    def test_boards(self):
+        boards = self.svc.boards()
+        assert len(boards) == 1
+        assert boards[0].name == "CI board"
+        assert boards[0].is_default is True
+
+    def test_jql_query(self):
+        boards = self.svc.boards()
+        query = self.svc.create_jql_query(board_keys=(board.key for board in boards))
+        assert "filter in" in query
+
+    @pytest.mark.usefixtures("issue")
+    def test_add_attachment(self):
+        file = werkzeug.datastructures.FileStorage(
+            stream=io.BytesIO(b"some dummy data"),
+            filename="dummy.txt",
+            content_type="text/plain",
+        )
+        attachment = self.svc.add_attachment(issue=self.issue, attachment=file)
+        issue = self.svc.issue(id=self.issue.id)
+        assert len(issue.fields.attachment) == 1
+        assert issue.fields.attachment[0].id == attachment.id
+        assert self.svc.attachment(id=attachment.id).get() == b"some dummy data"
+        issue.delete()
